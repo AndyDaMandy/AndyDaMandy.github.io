c4ff4b34012d41d0710ef91b5bad723eda704f64
Andy M <maldonado.andy.m@gmail.com>
2019-12-11 21:56:10 +0000

    I added an alert and some comments.

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+// Does this sync properly to github? Find out more on DRAGON BALL Z!!!
